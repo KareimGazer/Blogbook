04b404ecd699479809c9eceba8ce8a223c81776e
KareimGazer <kareimgazar1990@gmail.com>
2024-10-05 23:28:19 +0300

    added the blog model

---
+++ models/blog.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minLength: 5,
+  },
+    author: {
+        type: String,
+        required: true
+  },
+    url: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&\/=]*)$/.test(v)
+            },
+            message: props => `${props.value} is not a valid URL!`
+        }
+  },
+    likes: {
+        type: Number,
+        default: 0
+  }
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
