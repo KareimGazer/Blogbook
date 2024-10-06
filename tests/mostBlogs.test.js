985b9539a882c46fc84f869428c39bd7742a6842
KareimGazer <kareimgazar1990@gmail.com>
2024-10-06 23:21:56 +0300

    added mostBlogs for the top writer + tests

---
+++ tests/mostBlogs.test.js
@@ -0,0 +1,31 @@
+const {test, describe} = require('node:test')
+const assert = require('node:assert')
+const { mostBlogs } = require('../utils/list_helper')
+const { dummy_blogs } = require('./data')
+
+describe('most blogs', () => {
+    test('when list has only one blog, it is the author of that', () => {
+        const listWithOneBlog = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+                likes: 5,
+                __v: 0
+            }
+        ]
+        const result = mostBlogs(listWithOneBlog)
+        assert.deepStrictEqual(result, { author: 'Edsger W. Dijkstra', blogs: 1 })
+    })
+
+    test('when list has many blogs, it returns the author with most', () => {
+        const result = mostBlogs(dummy_blogs)
+        assert.deepStrictEqual(result, { author: 'Robert C. Martin', blogs: 3 })
+    })
+
+    test('when list is empty, it is null', () => {
+        const result = mostBlogs([])
+        assert.deepStrictEqual(result, null)
+    })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,11 +1,28 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
 const favoriteBlog = (blogs) => blogs.length === 0 ? null : blogs.reduce((a, b) => a.likes > b.likes ? a : b)
 
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    else {
+        const authors = _.countBy(blogs, 'author')
+        const most_auth = Object.keys(authors).reduce((a, b) => authors[a] > authors[b] ? a : b)
+        return {
+            author: most_auth,
+            blogs: authors[most_auth]
+        }
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
